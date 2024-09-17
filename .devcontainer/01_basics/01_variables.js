8f4850031593895ab9dcd08e146891e0d9d1d36b
paras <132212425+parasjain46@users.noreply.github.com>
2024-09-17 08:04:33 +0000

    datatype

---
+++ .devcontainer/01_basics/01_variables.js
@@ -0,0 +1,17 @@
+const accountId=144553
+let accountEmail="paras@gmail.com"
+var accountPassword="12345"
+accountCity="jaipur"
+//accountId=2;//not allow
+
+
+accountEmail="hcsd@gmail.com"
+accountPassword="123"
+accountCity="chennai"
+ let accountState;
+console.log(accountId);
+/*
+prefer not to use var
+beacause of issue block scope and functional scope
+*/
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState]);
---
+++ .devcontainer/01_basics/02_dataTypes.js
@@ -0,0 +1,17 @@
+"use strict";//treat all Js code as newer version
+//alert(3+3)//we are suing node js,not browser
+console.log(3+3);//code readability shoud be high
+console.log("Hitesh");
+let name="jitesh"
+let age=18
+let isLoggedIn=false
+//number =>2 to power 53
+//bigint
+//string=>""
+//boolean=>true/false
+//null=>object
+//undefined=>undefined
+//symbol=>unique
+//object=>
+console.log(typeof null);//object
+console.log(typeof undefined);//undefined
