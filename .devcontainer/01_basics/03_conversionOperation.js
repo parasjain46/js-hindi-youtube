bf90dc916355ee83fd2e42180d4384808903f993
paras <132212425+parasjain46@users.noreply.github.com>
2024-09-17 10:24:06 +0000

    conversion

---
+++ .devcontainer/01_basics/03_conversionOperation.js
@@ -0,0 +1,40 @@
+/*let score="33abc"
+console.log(typeof score);//String
+console.log(typeof(score));//String
+
+let valueInNumber=Number(score)
+console.log(typeof valueInNumber);//number
+
+console.log(valueInNumber);//nan*/
+
+/*let score="33"
+console.log(typeof score);//string
+console.log(typeof(score));//string
+
+let valueInNumber=Number(score)
+console.log(typeof valueInNumber);//number
+
+console.log(valueInNumber);//33*/
+
+/*let score=null
+console.log(typeof score);//object
+console.log(typeof(score));//object
+
+let valueInNumber=Number(score)
+console.log(typeof valueInNumber);//number
+
+console.log(valueInNumber);//0*/
+//"33"=>33
+//"33abc"=>Nan
+//true=>1;//false=>0
+let isLoggedIn=1
+let booleanIsLoggedIn=Boolean(isLoggedIn);
+console.log(booleanIsLoggedIn);
+//1=>true
+//0=>false
+//""=>false
+//"paras"=>true
+let someNumber=33
+let stringNumber=String(someNumber);
+console.log(stringNumber);
+console.log(typeof stringNumber);
