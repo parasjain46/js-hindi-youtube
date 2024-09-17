d3db1244b3f4e3ad89fbb534de2d8491b3e0ba5f
paras <132212425+parasjain46@users.noreply.github.com>
2024-09-17 10:53:22 +0000

    conversion

--- .devcontainer/01_basics/03_conversionOperation.js
+++ .devcontainer/01_basics/03_conversionOperation.js
@@ -27,14 +27,45 @@ console.log(valueInNumber);//0*/
 //"33"=>33
 //"33abc"=>Nan
 //true=>1;//false=>0
-let isLoggedIn=1
-let booleanIsLoggedIn=Boolean(isLoggedIn);
-console.log(booleanIsLoggedIn);
+// let isLoggedIn=1
+// let booleanIsLoggedIn=Boolean(isLoggedIn);
+// console.log(booleanIsLoggedIn);
 //1=>true
 //0=>false
 //""=>false
 //"paras"=>true
-let someNumber=33
-let stringNumber=String(someNumber);
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// let someNumber=33
+// let stringNumber=String(someNumber);
+// console.log(stringNumber);
+//console.log(typeof stringNumber);
+//***********Operation*********
+
+let value=3;
+let negValue=-value
+//console.log(negValue);-3
+// console.log(2+2);4
+// console.log(2-2);0
+// console.log(2*2);4
+// console.log(2**3);8
+// console.log(2/3);0.6666666666666666
+// console.log(2%3);2
+
+// let str1="hello"
+// let str2="hitesh"
+// let str3=str1+""+str2;
+// console.log(str3);
+// console.log("1"+2);
+// console.log(1+"2");
+// console.log("1"+2+2);
+// console.log(1+1+"2");
+
+//console.log(+true)//1
+//console.log(+"");0
+
+let num1,num2,num3
+
+num1=num2=num3=2+2;
+
+let gameCounter=100
+gameCounter++;
+console.log(gameCounter);
