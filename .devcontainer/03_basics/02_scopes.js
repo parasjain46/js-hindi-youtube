a121278b57b1d51a9bd55b68170c7bd63dba7317
paras <132212425+parasjain46@users.noreply.github.com>
2024-09-30 19:31:45 +0000

    scope

--- .devcontainer/03_basics/02_scopes.js
+++ .devcontainer/03_basics/02_scopes.js
@@ -5,7 +5,7 @@ if(true){
     let a=10
     const b=20
     c=20;
-    console.log("Inner: ",a);
+    // console.log("Inner: ",a);
 }
 
 // for(let i=0;i<Array.length;i++){
@@ -14,4 +14,16 @@ if(true){
 
 //console.log(a);
 //console.log(b);
-console.log(c);
+// console.log(c);
+
+
+function one(){
+    const username="hitesh"
+
+    function two(){
+        const website ="youtube"
+        console.log(username);
+    }
+    console.log(website);
+    two();
+}
