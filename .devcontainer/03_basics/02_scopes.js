0f3942be8835db6a82f1be58387ac476bfc310e4
paras <132212425+parasjain46@users.noreply.github.com>
2024-10-01 09:30:15 +0000

    sc

--- .devcontainer/03_basics/02_scopes.js
+++ .devcontainer/03_basics/02_scopes.js
@@ -18,12 +18,39 @@ if(true){
 
 
 function one(){
-    const username="hitesh"
+    const username="paras"
 
     function two(){
         const website ="youtube"
         console.log(username);
     }
-    console.log(website);
+    //console.log(website);
     two();
 }
+//one();
+
+if(true){
+    const username="paras"
+    if(username==="paras")
+    {
+        const website="youtube"
+        //console.log(username+website);  
+      }
+      //console.log(website);
+}
+//console.log(username);
+
+
+//++++++++++++++++++++++++++
+
+addone(5)
+function addone(num){
+    return num+1
+}
+
+
+addTwo(5)
+const addTwo=function(num){
+    return num+2
+}
+
