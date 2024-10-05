2d4edc1231a8bbed9aae1f88092cbd6249f53bfb
paras <132212425+parasjain46@users.noreply.github.com>
2024-10-05 09:03:45 +0000

    if

---
+++ .devcontainer/04_control_flow/one.js
@@ -0,0 +1,47 @@
+//if
+// const isUserloggedIn=true
+// const temperature=41
+
+// if(temperature===41){
+// console.log("less");
+// }else{
+//    console.log("great") 
+// }
+//<,>,<=,>=,==,!=,===,!==
+// const score=200;
+// if(score>100){
+//      power="fly"
+//     console.log(`User power: ${power}`);
+// }
+// console.log(`User power: ${power}`);
+
+const bal=1000;
+//if(ball>500)console.log("test")
+
+    // if(bal<500){
+    //     console.log("less than 500");
+    // }
+    // else if(bal<750){
+    //     console.log("less than 750");
+    // }else if(bal<900){
+    //     console.log("less than 900");
+    // }else{
+    //     console.log("less than 1200");
+    // }
+
+
+
+    // const userLoggedIn=true
+    // const debitCard=true
+    // const loggedInFromGoogle=false
+    // const loggedInFromEmail=true
+
+    // if(userLoggedIn&&debitCard){
+    //     console.log("Allow to buy course");
+    // }
+
+    // if(loggedInFromEmail||loggedInFromGoogle){
+    //     console.log("User logged in");
+    // }
+
+   
